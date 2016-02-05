f38696a65e1668175d568188433e6779a990d0db
hojin chu <chjin990@gmail.com>
2016-02-05 17:48:43 +0900

    마우스 아이콘 이미지 키우기 테스트

--- src/com/client/GameRule.java
+++ src/com/client/GameRule.java
@@ -1,22 +1,31 @@
 package com.client;
 import javax.swing.*;
 import java.awt.*;
-public class GameRule extends JPanel{
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.image.BufferedImage;
+public class GameRule extends JPanel implements Runnable, MouseMotionListener{
 	
-	Image back, img;		//마우스 그림 추가
+	Image imgGround, img;		//마우스 그림 추가
 	JButton b1;//버튼     
 	Cursor cursor;			//커서 추가
 	
+	//타이머와 스레드 추가
+	//Timer timer=new Timer(10,null);
+	//Thread thread;
+	
+	// 변수 선언-용도: 이미지 영역내에서 마우스 해머크기가 크게 나오게 하기
+	int hammerX, hammerY;
 	
 	
 	public GameRule()
 	{	
-		//마우스 그림 초기화
-		img=Toolkit.getDefaultToolkit().getImage("image/01.png");
-		cursor=Toolkit.getDefaultToolkit().createCustomCursor(img, new Point(0,0), "null");
-		this.setCursor(cursor);
-
-		back = Toolkit.getDefaultToolkit().getImage("image/gr.png");
+		//마우스모션 리스너 추가
+		this.addMouseMotionListener(this);
+		
+		//그림 초기화
+		imgGround = Toolkit.getDefaultToolkit().getImage("image/gr.png");
+		img=Toolkit.getDefaultToolkit().getImage("image/01.png");	
 	
 		b1=new JButton("돌아가기 >>");
 		
@@ -33,7 +42,28 @@ public class GameRule extends JPanel{
 	@Override	//
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(back, 0, 0, getWidth(), getHeight(), this);
+		g.drawImage(imgGround, 0, 0, getWidth(), getHeight(), this);
+		g.drawImage(img, hammerX-35, hammerY-35, 70,70,this);
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		this.setCursor(this.getToolkit().createCustomCursor(new BufferedImage(3,3,BufferedImage.TYPE_INT_ARGB), new Point(0,0), "null"));
+		hammerX=e.getX();
+		hammerY=e.getY();
+		repaint();
+		
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		this.setCursor(this.getToolkit().createCustomCursor(new BufferedImage(3,3,BufferedImage.TYPE_INT_ARGB), new Point(0,0), "null"));
+		hammerX=e.getX();
+		hammerY=e.getY();
+		repaint();
 	}
 }
 
